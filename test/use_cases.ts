d0f4ba004d0cc1887486e473ed193b1f2bde3658
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2023-07-04 17:34:28 -0300

    Update use_cases.ts

--- test/use_cases.ts
+++ test/use_cases.ts
@@ -4,7 +4,7 @@ import { MockUserRepo } from "../repos/mock/mockUserRepo";
   describe('createUser', () => {
     test('deve criar um novo usuario', async () => {
     
-      const createUser: user = { id: 1, nome: 'johnDoe', password: 'secretpassword'};
+      const createUser: user = { id: 1, login: 'johnDoe', password: 'secretpassword'};
 
       const newUser = await userRepo.createUser(id);
 
