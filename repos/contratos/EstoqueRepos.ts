80f6bad61358953bb2e000731b1b53b2a89dbe0f
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2023-08-08 17:58:18 -0300

    Delete EstoqueRepos.ts

--- repos/contratos/EstoqueRepos.ts
+++
@@ -1,9 +0,0 @@
-import {estoque} from '../../entidades/estoque';
-
-export interface estoqueRepos {
-    criarEstoque(estoque: estoque): Promise<estoque>;
-    obterEstoque(): Promise<estoque[]>;
-    obterEstoquePorId(id: string): Promise<estoque | null>;
-    atualizarEstoque(estoque: estoque): Promise<estoque>;
-    excluirEstoque(id: string): Promise<boolean>;
-}
