10d10731dcf2e0a129a22733a956ba99aec9cdd8
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2023-08-08 17:58:36 -0300

    Delete ProdutoRepos.ts

--- repos/contratos/ProdutoRepos.ts
+++
@@ -1,9 +0,0 @@
-import {produto} from '../../entidades/produto';
-
-export interface produtoRepos  {
-  criarProduto(produto: produto): Promise<produto>;
-  obterProduto(): Promise<produto[]>;
-  obterProdutoPorId(id: string): Promise<produto | null>;
-  atualizarProduto(produto: produto): Promise<produto>;
-  excluirProduto(id: string): Promise<boolean>;
-}
