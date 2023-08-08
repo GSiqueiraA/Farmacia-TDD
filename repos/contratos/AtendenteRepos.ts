3203c682872b4196097667ed1310c81c9481e563
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2023-08-08 17:57:53 -0300

    Delete AtendenteRepos.ts

--- repos/contratos/AtendenteRepos.ts
+++
@@ -1,9 +0,0 @@
-import {atendente} from '../../entidades/atendente';
-
-export interface atendenteRepos  {
-  criarAtendente(atendente: atendente): Promise<atendente>;
-  obterAtendente(): Promise<atendente[]>;
-  obterAtendentePorId(id: string): Promise<atendente | null>;
-  atualizarAtendente(atendente: atendente): Promise<atendente>;
-  excluirAtendente(id: string): Promise<boolean>;
-}
