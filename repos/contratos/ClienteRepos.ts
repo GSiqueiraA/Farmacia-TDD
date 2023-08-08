b55b25a5c12c5f9e74732ebbf3c4f561e59285dd
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2023-08-08 17:58:05 -0300

    Delete ClienteRepos.ts

--- repos/contratos/ClienteRepos.ts
+++
@@ -1,9 +0,0 @@
-import {cliente} from '../../entidades/cliente';
-
-export interface clienteRepos  {
-  criarCliente(cliente: cliente): Promise<cliente>;
-  obterCliente(): Promise<cliente[]>;
-  obterClientePorId(id: string): Promise<cliente | null>;
-  atualizarCliente(cliente: cliente): Promise<cliente>;
-  excluirCliente(id: string): Promise<boolean>;
-}
