287803d393bdd7477319a957c86f5610b37202db
GSiqueiraA <90071227+GSiqueiraA@users.noreply.github.com>
2023-08-08 17:58:44 -0300

    Delete UsuarioRepos.ts

--- repos/contratos/UsuarioRepos.ts
+++
@@ -1,9 +0,0 @@
-import {usuario} from '../../entidades/usuario';
-
-export interface usuarioRepos {
-    criarUsuario(usuario: usuario): Promise<usuario>;
-    obterUsuario(): Promise<usuario[]>;
-    obterLoginPorId(id: string): Promise<usuario | null>;
-    atualizarUsuario(usuario: usuario): Promise<usuario>;
-    excluirUsuario(id: string): Promise<boolean>;
-}
